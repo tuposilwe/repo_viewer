41e0ab95fb69cdaa5c53a282dea49b3e85d9994b
tuposilwe <rudigermkondya@gmail.com>
2026-05-22 07:41:40 +0300

    Changing the Access Token Response Format To JSON

--- lib/auth/infrastructure/github_authenticator.dart
+++ lib/auth/infrastructure/github_authenticator.dart
@@ -1,10 +1,21 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:dartz/dartz.dart';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 import 'package:oauth2/oauth2.dart';
 import 'package:repo_viewer/auth/domain/auth_failure.dart';
-
 import 'package:repo_viewer/auth/infrastructure/credentials_storage/credentials_storage.dart';
+
+class GithubOauthHttpClient  extends http.BaseClient{
+  final httpClient =  http.Client();
+  
+  @override
+  Future<http.StreamedResponse> send(http.BaseRequest request) {
+    request.headers['Accept'] = 'application/json';
+    return httpClient.send(request);
+  }
+  
+}
 
 class GithubAuthenticator {
   final CredentialsStorage _credentialsStorage;
@@ -50,6 +61,7 @@ class GithubAuthenticator {
       authorizationEndpoint,
       tokenEndpoint,
       secret: clientSecret,
+      httpClient: GithubOauthHttpClient()
     );
   }
 
