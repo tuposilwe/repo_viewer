b59e5fcec1c51c89dbf388faa6fd4ed580b6e0f8
tuposilwe <rudigermkondya@gmail.com>
2026-05-22 09:27:23 +0300

    Signing Out and Token Revocation

--- lib/auth/infrastructure/github_authenticator.dart
+++ lib/auth/infrastructure/github_authenticator.dart
@@ -1,26 +1,28 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:oauth2/oauth2.dart';
 import 'package:repo_viewer/auth/domain/auth_failure.dart';
 import 'package:repo_viewer/auth/infrastructure/credentials_storage/credentials_storage.dart';
+import 'package:repo_viewer/core/shared/encoders.dart';
 
-class GithubOauthHttpClient  extends http.BaseClient{
-  final httpClient =  http.Client();
-  
+class GithubOauthHttpClient extends http.BaseClient {
+  final httpClient = http.Client();
+
   @override
   Future<http.StreamedResponse> send(http.BaseRequest request) {
     request.headers['Accept'] = 'application/json';
     return httpClient.send(request);
   }
-  
 }
 
 class GithubAuthenticator {
   final CredentialsStorage _credentialsStorage;
+  final Dio _dio;
 
-  GithubAuthenticator(this._credentialsStorage);
+  GithubAuthenticator(this._credentialsStorage, this._dio);
 
   static const clientId = 'Ov23ct7mqD41IrZbosKN';
   static const clientSecret = '5575e107224a88a1db3420469d845a197d62c7ee';
@@ -28,6 +30,10 @@ class GithubAuthenticator {
 
   static final authorizationEndpoint = Uri.parse(
     'https://github.com/login/oauth/authorize',
+  );
+
+  static final revocationEndpoint = Uri.parse(
+    'https://api.github.com/applications/$clientId/token',
   );
 
   static final tokenEndpoint = Uri.parse(
@@ -61,7 +67,7 @@ class GithubAuthenticator {
       authorizationEndpoint,
       tokenEndpoint,
       secret: clientSecret,
-      httpClient: GithubOauthHttpClient()
+      httpClient: GithubOauthHttpClient(),
     );
   }
 
@@ -81,6 +87,30 @@ class GithubAuthenticator {
       return left(const AuthFailure.server());
     } on AuthorizationException catch (e) {
       return left(AuthFailure.server('${e.error}: ${e.description}'));
+    } on PlatformException {
+      return left(const AuthFailure.storage());
+    }
+  }
+
+  Future<Either<AuthFailure, Unit>> signOut() async {
+    final accessToken = await _credentialsStorage.read().then(
+      (credentials) => credentials?.accessToken,
+    );
+
+    final usernameAndPassword = stringToBase64.encode(
+      '$clientId:$clientSecret',
+    );
+
+    try {
+      _dio.deleteUri(
+        revocationEndpoint,
+        data: {'access_token': accessToken},
+        options: Options(
+          headers: {'Authorization': 'basic $usernameAndPassword'},
+        ),
+      );
+      await _credentialsStorage.clear();
+      return right(unit);
     } on PlatformException {
       return left(const AuthFailure.storage());
     }
