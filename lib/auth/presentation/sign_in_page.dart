3893e23cb182f768d1df954b9d3e5eb0f01eb753
tuposilwe <rudigermkondya@gmail.com>
2026-05-23 22:12:43 +0300

    Building the Sign In Page UI

--- lib/auth/presentation/sign_in_page.dart
+++ lib/auth/presentation/sign_in_page.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 @RoutePage()
 class SignInPage extends StatelessWidget {
@@ -7,6 +8,39 @@ class SignInPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 48.0),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Icon(MdiIcons.github, size: 150),
+                  const SizedBox(height: 16),
+                  Text(
+                    'Welcome to \nrepo Viewer',
+              
+                    style: Theme.of(context).textTheme.headlineLarge,
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 32),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.green),
+                      foregroundColor:MaterialStateProperty.all( Colors.white)
+                    ),
+                    child: Text('Sign In'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
