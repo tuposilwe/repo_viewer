9ae58e44900147074e6cf643a22aec1c1871dcc8
tuposilwe <rudigermkondya@gmail.com>
2026-05-26 19:26:25 +0300

    Creating a RemoteResponse Union

---
+++ lib/core/infrastructure/remote_response.freezed.dart
@@ -0,0 +1,316 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'remote_response.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+// dart format off
+T _$identity<T>(T value) => value;
+/// @nodoc
+mixin _$RemoteResponse<T> {
+
+
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is RemoteResponse<T>);
+}
+
+
+@override
+int get hashCode => runtimeType.hashCode;
+
+@override
+String toString() {
+  return 'RemoteResponse<$T>()';
+}
+
+
+}
+
+/// @nodoc
+class $RemoteResponseCopyWith<T,$Res>  {
+$RemoteResponseCopyWith(RemoteResponse<T> _, $Res Function(RemoteResponse<T>) __);
+}
+
+
+/// Adds pattern-matching-related methods to [RemoteResponse].
+extension RemoteResponsePatterns<T> on RemoteResponse<T> {
+/// A variant of `map` that fallback to returning `orElse`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
+
+@optionalTypeArgs TResult maybeMap<TResult extends Object?>({TResult Function( _NoConnection<T> value)?  noConnection,TResult Function( _NotModified<T> value)?  _notModified,TResult Function( _WithNewData<T> value)?  withNewData,required TResult orElse(),}){
+final _that = this;
+switch (_that) {
+case _NoConnection() when noConnection != null:
+return noConnection(_that);case _NotModified() when _notModified != null:
+return _notModified(_that);case _WithNewData() when withNewData != null:
+return withNewData(_that);case _:
+  return orElse();
+
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// Callbacks receives the raw object, upcasted.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case final Subclass2 value:
+///     return ...;
+/// }
+/// ```
+
+@optionalTypeArgs TResult map<TResult extends Object?>({required TResult Function( _NoConnection<T> value)  noConnection,required TResult Function( _NotModified<T> value)  _notModified,required TResult Function( _WithNewData<T> value)  withNewData,}){
+final _that = this;
+switch (_that) {
+case _NoConnection():
+return noConnection(_that);case _NotModified():
+return _notModified(_that);case _WithNewData():
+return withNewData(_that);case _:
+  throw StateError('Unexpected subclass');
+
+}
+}
+/// A variant of `map` that fallback to returning `null`.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case final Subclass value:
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
+
+@optionalTypeArgs TResult? mapOrNull<TResult extends Object?>({TResult? Function( _NoConnection<T> value)?  noConnection,TResult? Function( _NotModified<T> value)?  _notModified,TResult? Function( _WithNewData<T> value)?  withNewData,}){
+final _that = this;
+switch (_that) {
+case _NoConnection() when noConnection != null:
+return noConnection(_that);case _NotModified() when _notModified != null:
+return _notModified(_that);case _WithNewData() when withNewData != null:
+return withNewData(_that);case _:
+  return null;
+
+}
+}
+/// A variant of `when` that fallback to an `orElse` callback.
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return orElse();
+/// }
+/// ```
+
+@optionalTypeArgs TResult maybeWhen<TResult extends Object?>({TResult Function()?  noConnection,TResult Function()?  _notModified,TResult Function( T data)?  withNewData,required TResult orElse(),}) {final _that = this;
+switch (_that) {
+case _NoConnection() when noConnection != null:
+return noConnection();case _NotModified() when _notModified != null:
+return _notModified();case _WithNewData() when withNewData != null:
+return withNewData(_that.data);case _:
+  return orElse();
+
+}
+}
+/// A `switch`-like method, using callbacks.
+///
+/// As opposed to `map`, this offers destructuring.
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case Subclass2(:final field2):
+///     return ...;
+/// }
+/// ```
+
+@optionalTypeArgs TResult when<TResult extends Object?>({required TResult Function()  noConnection,required TResult Function()  _notModified,required TResult Function( T data)  withNewData,}) {final _that = this;
+switch (_that) {
+case _NoConnection():
+return noConnection();case _NotModified():
+return _notModified();case _WithNewData():
+return withNewData(_that.data);case _:
+  throw StateError('Unexpected subclass');
+
+}
+}
+/// A variant of `when` that fallback to returning `null`
+///
+/// It is equivalent to doing:
+/// ```dart
+/// switch (sealedClass) {
+///   case Subclass(:final field):
+///     return ...;
+///   case _:
+///     return null;
+/// }
+/// ```
+
+@optionalTypeArgs TResult? whenOrNull<TResult extends Object?>({TResult? Function()?  noConnection,TResult? Function()?  _notModified,TResult? Function( T data)?  withNewData,}) {final _that = this;
+switch (_that) {
+case _NoConnection() when noConnection != null:
+return noConnection();case _NotModified() when _notModified != null:
+return _notModified();case _WithNewData() when withNewData != null:
+return withNewData(_that.data);case _:
+  return null;
+
+}
+}
+
+}
+
+/// @nodoc
+
+
+class _NoConnection<T> extends RemoteResponse<T> {
+  const _NoConnection(): super._();
+  
+
+
+
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _NoConnection<T>);
+}
+
+
+@override
+int get hashCode => runtimeType.hashCode;
+
+@override
+String toString() {
+  return 'RemoteResponse<$T>.noConnection()';
+}
+
+
+}
+
+
+
+
+/// @nodoc
+
+
+class _NotModified<T> extends RemoteResponse<T> {
+  const _NotModified(): super._();
+  
+
+
+
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _NotModified<T>);
+}
+
+
+@override
+int get hashCode => runtimeType.hashCode;
+
+@override
+String toString() {
+  return 'RemoteResponse<$T>._notModified()';
+}
+
+
+}
+
+
+
+
+/// @nodoc
+
+
+class _WithNewData<T> extends RemoteResponse<T> {
+  const _WithNewData(this.data): super._();
+  
+
+ final  T data;
+
+/// Create a copy of RemoteResponse
+/// with the given fields replaced by the non-null parameter values.
+@JsonKey(includeFromJson: false, includeToJson: false)
+@pragma('vm:prefer-inline')
+_$WithNewDataCopyWith<T, _WithNewData<T>> get copyWith => __$WithNewDataCopyWithImpl<T, _WithNewData<T>>(this, _$identity);
+
+
+
+@override
+bool operator ==(Object other) {
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is _WithNewData<T>&&const DeepCollectionEquality().equals(other.data, data));
+}
+
+
+@override
+int get hashCode => Object.hash(runtimeType,const DeepCollectionEquality().hash(data));
+
+@override
+String toString() {
+  return 'RemoteResponse<$T>.withNewData(data: $data)';
+}
+
+
+}
+
+/// @nodoc
+abstract mixin class _$WithNewDataCopyWith<T,$Res> implements $RemoteResponseCopyWith<T, $Res> {
+  factory _$WithNewDataCopyWith(_WithNewData<T> value, $Res Function(_WithNewData<T>) _then) = __$WithNewDataCopyWithImpl;
+@useResult
+$Res call({
+ T data
+});
+
+
+
+
+}
+/// @nodoc
+class __$WithNewDataCopyWithImpl<T,$Res>
+    implements _$WithNewDataCopyWith<T, $Res> {
+  __$WithNewDataCopyWithImpl(this._self, this._then);
+
+  final _WithNewData<T> _self;
+  final $Res Function(_WithNewData<T>) _then;
+
+/// Create a copy of RemoteResponse
+/// with the given fields replaced by the non-null parameter values.
+@pragma('vm:prefer-inline') $Res call({Object? data = freezed,}) {
+  return _then(_WithNewData<T>(
+freezed == data ? _self.data : data // ignore: cast_nullable_to_non_nullable
+as T,
+  ));
+}
+
+
+}
+
+// dart format on
