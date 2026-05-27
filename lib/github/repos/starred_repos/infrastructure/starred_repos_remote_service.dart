6ad44fa4b14645f07703618c7dc12d18f293a743
tuposilwe <rudigermkondya@gmail.com>
2026-05-27 14:32:51 +0300

    Caching the GitHub Headers

--- lib/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart
+++ lib/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart
@@ -1,12 +1,20 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:dio/dio.dart';
+
 import 'package:repo_viewer/core/infrastructure/remote_response.dart';
+import 'package:repo_viewer/github/core/infrastructure/github_headers_cache.dart';
 import 'package:repo_viewer/github/core/infrastructure/github_repo_dto.dart';
 
 class StarredReposRemoteService {
   final Dio _dio;
+  final GithubHeadersCache _headersCache;
+  
+  const StarredReposRemoteService(
+    this._dio,
+    this._headersCache,
+  );
 
-  StarredReposRemoteService(this._dio);
+  Future<RemoteResponse<List<GithubRepoDto>>> getStarredReposPage(int page) async {
 
-  Future<RemoteResponse<List<GithubRepoDto>>> getStarredReposPage(int page) async {}
+  }
 }
- 
