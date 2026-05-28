6da8ec14256bba7b63a327949bf9cb08e62bff83
tuposilwe <rudigermkondya@gmail.com>
2026-05-28 18:32:36 +0300

    StarredReposLocalService Reading a Page

--- lib/github/repos/starred_repos/infrastructure/starred_repos_local_services.dart
+++ lib/github/repos/starred_repos/infrastructure/starred_repos_local_services.dart
@@ -1,6 +1,7 @@
 import 'package:collection/collection.dart';
 import 'package:repo_viewer/core/infrastructure/sembast_database.dart';
 import 'package:repo_viewer/github/core/infrastructure/github_repo_dto.dart';
+import 'package:repo_viewer/github/core/infrastructure/pagination_config.dart';
 import 'package:sembast/sembast_io.dart';
 
 class StarredReposLocalServices {
@@ -14,9 +15,25 @@ class StarredReposLocalServices {
     final sembastPage = page - 1;
 
     await _store
-      .records(dtos.mapIndexed((index,_) => index + 3 * sembastPage))
-      .put(_sembastDatabase.instance, 
-      dtos.map((e) => e.toJson()).toList()
-      );
+        .records(
+          dtos.mapIndexed(
+            (index, _) => index + PaginationConfig.itemsPerPage * sembastPage,
+          ),
+        )
+        .put(_sembastDatabase.instance, dtos.map((e) => e.toJson()).toList());
+  }
+
+  Future<List<GithubRepoDto>> getPage(int page) async {
+    final sembastPage = page - 1;
+
+    final records = await _store.find(
+      _sembastDatabase.instance,
+      finder: Finder(
+        limit: PaginationConfig.itemsPerPage,
+        offset: PaginationConfig.itemsPerPage * sembastPage,
+      ),
+    );
+
+    return records.map((e) => GithubRepoDto.fromJson(e.value)).toList();
   }
 }
--- lib/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart
+++ lib/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart
@@ -7,6 +7,7 @@ import 'package:repo_viewer/core/infrastructure/remote_response.dart';
 import 'package:repo_viewer/github/core/infrastructure/github_headers.dart';
 import 'package:repo_viewer/github/core/infrastructure/github_headers_cache.dart';
 import 'package:repo_viewer/github/core/infrastructure/github_repo_dto.dart';
+import 'package:repo_viewer/github/core/infrastructure/pagination_config.dart';
 
 class StarredReposRemoteService {
   final Dio _dio;
@@ -22,7 +23,10 @@ class StarredReposRemoteService {
     const acceptHeader = "application/vnd.github+json";
     // const readmeAcceptHeader = "application/vnd.github.v3.html+json";
 
-    final requestUri = Uri.https(baseUrl, '/user/starred', {'page': '$page'});
+    final requestUri = Uri.https(baseUrl, '/user/starred', {
+      'page': '$page',
+      'per_page': PaginationConfig.itemsPerPage.toString(),
+    });
 
     final previousHeaders = await _headersCache.getHeaders(requestUri);
 
