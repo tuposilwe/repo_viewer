7a300bc7a5ffe9505bc9dd07e5b8b294c0f2ebe2
tuposilwe <rudigermkondya@gmail.com>
2026-05-28 07:33:38 +0300

    StarredReposRepository Implementation Part 1 Using Only Remote Service

--- lib/github/repos/starred_repos/infrastructure/starred_repos_repository.dart
+++ lib/github/repos/starred_repos/infrastructure/starred_repos_repository.dart
@@ -1,6 +1,10 @@
 import 'package:dartz/dartz.dart';
+import 'package:repo_viewer/core/domain/fresh.dart';
+import 'package:repo_viewer/core/infrastructure/network_exceptions.dart';
+import 'package:repo_viewer/core/infrastructure/remote_response.dart';
 import 'package:repo_viewer/github/core/domain/github_failure.dart';
 import 'package:repo_viewer/github/core/domain/github_repo.dart';
+import 'package:repo_viewer/github/core/infrastructure/github_repo_dto.dart';
 import 'package:repo_viewer/github/repos/starred_repos/infrastructure/starred_repos_remote_service.dart';
 
 class StarredReposRepository {
@@ -9,7 +13,36 @@ class StarredReposRepository {
   // TODO: local service
   StarredReposRepository(this._remoteService);
 
-  Future<Either<GithubFailure, List<GithubRepo>>> getStarredReposPage(
+  Future<Either<GithubFailure, Fresh<List<GithubRepo>>>> getStarredReposPage(
     int page,
-  ) async {}
+  ) async {
+    try {
+      final remotePageItems = await _remoteService.getStarredReposPage(page);
+      return right(
+        remotePageItems.when(
+          // TODO: local service
+          noConnection: (maxPage) =>
+              Fresh.no([], isNextPageAvailable: page < maxPage),
+          // TODO: local service
+          notModified: (maxPage) =>
+              Fresh.yes([], isNextPageAvailable: page < maxPage),
+          withNewData: (data, maxPage) {
+          // TODO: save data in local service
+            return Fresh.yes(
+              data.toDomain(),
+              isNextPageAvailable: page < maxPage,
+            );
+          },
+        ),
+      );
+    } on RestAPiException catch (e) {
+      return left(GithubFailure.api(e.errorCode));
+    }
+  }
+}
+
+extension DTOListToDomainList on List<GithubRepoDto> {
+  List<GithubRepo> toDomain() {
+    return map((e) => e.toDomain()).toList();
+  }
 }
