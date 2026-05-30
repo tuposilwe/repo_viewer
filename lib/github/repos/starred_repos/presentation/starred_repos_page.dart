4b74fecf88554bf090d73f8049d659c5265e4692
tuposilwe <rudigermkondya@gmail.com>
2026-05-30 07:32:37 +0300

    Initial Creation of the PaginatedReposListView

--- lib/github/repos/starred_repos/presentation/starred_repos_page.dart
+++ lib/github/repos/starred_repos/presentation/starred_repos_page.dart
@@ -1,27 +1,56 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:repo_viewer/auth/shared/providers.dart';
+import 'package:repo_viewer/github/core/shared/providers.dart';
 
 @RoutePage()
-class StarredReposPage extends ConsumerWidget {
+class StarredReposPage extends ConsumerStatefulWidget {
   const StarredReposPage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            ref.read(authNotifierProvider.notifier).signOut();
-          },
-          style: ButtonStyle(
-            backgroundColor: WidgetStateProperty.all(Colors.red),
-            foregroundColor: WidgetStateProperty.all(Colors.white),
-          ),
-          child: const Text('Sign Out'),
-        ),
-      ),
+  ConsumerState<StarredReposPage> createState() => _StarredReposPageState();
+}
+
+class _StarredReposPageState extends ConsumerState<StarredReposPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.microtask(
+      () => ref
+          .read(starredReposNotifierProvider.notifier)
+          .getNextStarredReposPage(),
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Starred Repos'),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        foregroundColor: Theme.of(context).colorScheme.onPrimary,
+        actions: [
+          IconButton(
+            onPressed: () {
+              ref.read(authNotifierProvider.notifier).signOut();
+            },
+            icon: Icon(MdiIcons.logoutVariant),
+          ),
+        ],
+      ),
+      body: const PaginatedReposListView(),
+    );
+  }
+}
+
+class PaginatedReposListView extends StatelessWidget {
+  const PaginatedReposListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView();
   }
 }
