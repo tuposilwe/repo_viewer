e7b486a89a9797ce3bfe8743ed6a678238f5d131
tuposilwe <rudigermkondya@gmail.com>
2026-06-01 17:42:11 +0300

    Adding Pagination Part 1

--- lib/github/repos/starred_repos/presentation/paginated_repos_list_view.dart
+++ lib/github/repos/starred_repos/presentation/paginated_repos_list_view.dart
@@ -6,40 +6,69 @@ import 'package:repo_viewer/github/repos/starred_repos/presentation/failure_repo
 import 'package:repo_viewer/github/repos/starred_repos/presentation/loading_repo_tile.dart';
 import 'package:repo_viewer/github/repos/starred_repos/presentation/repo_tile.dart';
 
-class PaginatedReposListView extends StatelessWidget {
+class PaginatedReposListView extends StatefulWidget {
   const PaginatedReposListView({super.key});
+
+  @override
+  State<PaginatedReposListView> createState() => _PaginatedReposListViewState();
+}
+
+class _PaginatedReposListViewState extends State<PaginatedReposListView> {
+  bool canLoadNextPage = false;
 
   @override
   Widget build(BuildContext context) {
     return Consumer(
       builder: (context, ref, child) {
         final state = ref.watch(starredReposNotifierProvider);
-        return ListView.builder(
-          itemCount: state.map(
-            initial: (_) => 0,
-            loadInProgress: (e) => e.repos.entity.length + e.itemsPerPage,
-            loadSuccess: (e) => e.repos.entity.length,
-            loadFailure: (e) => e.repos.entity.length + 1,
-          ),
-          itemBuilder: (context, index) {
-            return state.map(
-              initial: (e) => RepoTile(repo: e.repos.entity[index]),
-              loadInProgress: (e) {
-                if (index < e.repos.entity.length) {
-                  return RepoTile(repo: e.repos.entity[index]);
-                } else {
-                  return const LoadingRepoTile();
-                }
-              },
-              loadSuccess: (e) => RepoTile(repo: e.repos.entity[index]),
-              loadFailure: (e) {
-                if (index < e.repos.entity.length) {
-                  return RepoTile(repo: e.repos.entity[index]);
-                } else {
-                  return FailureRepoTile(failure: e.failure);
-                }
-              },
-            );
+        ref.listen<StarredReposState>(starredReposNotifierProvider, (
+          previous,
+          state,
+        ) {
+          state.map(
+            initial: (e) => canLoadNextPage = true,
+            loadInProgress:  (e) => canLoadNextPage = false,
+            loadSuccess:  (e) => canLoadNextPage = e.isNextPageAvailable,
+            loadFailure:  (e) => canLoadNextPage = false,
+          );
+        });
+        return _PaginatedListView(state: state);
+      },
+    );
+  }
+}
+
+class _PaginatedListView extends StatelessWidget {
+  const _PaginatedListView({required this.state});
+
+  final StarredReposState state;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: state.map(
+        initial: (_) => 0,
+        loadInProgress: (e) => e.repos.entity.length + e.itemsPerPage,
+        loadSuccess: (e) => e.repos.entity.length,
+        loadFailure: (e) => e.repos.entity.length + 1,
+      ),
+      itemBuilder: (context, index) {
+        return state.map(
+          initial: (e) => RepoTile(repo: e.repos.entity[index]),
+          loadInProgress: (e) {
+            if (index < e.repos.entity.length) {
+              return RepoTile(repo: e.repos.entity[index]);
+            } else {
+              return const LoadingRepoTile();
+            }
+          },
+          loadSuccess: (e) => RepoTile(repo: e.repos.entity[index]),
+          loadFailure: (e) {
+            if (index < e.repos.entity.length) {
+              return RepoTile(repo: e.repos.entity[index]);
+            } else {
+              return FailureRepoTile(failure: e.failure);
+            }
           },
         );
       },
