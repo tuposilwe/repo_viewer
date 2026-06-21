6c7632da43da298a01fd0fd6efa93b599ec76255
tuposilwe <rudigermkondya@gmail.com>
2026-06-21 19:20:37 +0300

    Adding Parameters to the Search Bar

--- lib/github/repos/starred_repos/presentation/starred_repos_page.dart
+++ lib/github/repos/starred_repos/presentation/starred_repos_page.dart
@@ -2,11 +2,12 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-import 'package:material_floating_search_bar_plus/material_floating_search_bar_plus.dart';
+
 import 'package:repo_viewer/auth/shared/providers.dart';
 import 'package:repo_viewer/core/presentation/routes/app_router.dart';
 import 'package:repo_viewer/github/core/shared/providers.dart';
 import 'package:repo_viewer/github/repos/core/application/paginated_repos_list_view.dart';
+import 'package:repo_viewer/search/presentation/search_bar.dart';
 
 @RoutePage()
 class StarredReposPage extends ConsumerStatefulWidget {
@@ -30,33 +31,21 @@ class _StarredReposPageState extends ConsumerState<StarredReposPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Starred Repos'),
-        centerTitle: true,
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        foregroundColor: Theme.of(context).colorScheme.onPrimary,
-        actions: [
-          IconButton(
-            onPressed: () {
-              ref.read(authNotifierProvider.notifier).signOut();
-            },
-            icon: Icon(MdiIcons.logoutVariant),
-          ),
-          IconButton(
-            onPressed: () {
-              AutoRouter.of(
-                context,
-              ).push(SearchedReposRoute(searchTerm: 'angular'));
-            },
-            icon: Icon(MdiIcons.magnify),
-          ),
-        ],
-      ),
-      body: FloatingSearchBar(
-         hint: 'Search...',
-        builder: (context, transition) {
-          return Container();
+      body: SearchBarCustom(
+        title: 'Starred repositories',
+        hint: 'Search all repositories...',
+        onShouldNavigateToResultPage: (searchTerm) {
+          AutoRouter.of(
+            context,
+          ).push(SearchedReposRoute(searchTerm: searchTerm));
         },
+        onSignOutButtonPressed: (){
+           ref.read(authNotifierProvider.notifier).signOut();
+        },
+
+        // builder: (context, transition) {
+        //   return Container();
+        // },
         body: PaginatedReposListView(
           paginatedReposNotifierProvider: starredReposNotifierProvider,
           getNextPage: (ref) {
--- lib/search/presentation/search_bar.dart
+++ lib/search/presentation/search_bar.dart
@@ -1,15 +1,30 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
 import 'package:repo_viewer/search/shared/providers.dart';
 
-class SearchBar extends ConsumerStatefulWidget {
-  const SearchBar({super.key});
+class SearchBarCustom extends ConsumerStatefulWidget {
+  final Widget body;
+  final String title;
+  final String hint;
+  final void Function(String searchTerm) onShouldNavigateToResultPage;
+  final void Function() onSignOutButtonPressed;
+
+  const SearchBarCustom({
+    super.key,
+    required this.body,
+    required this.title,
+    required this.hint,
+    required this.onShouldNavigateToResultPage,
+    required this.onSignOutButtonPressed
+  });
 
   @override
-  ConsumerState<SearchBar> createState() => _SearchBarState();
+  ConsumerState<SearchBarCustom> createState() => _SearchBarState();
 }
 
-class _SearchBarState extends ConsumerState<SearchBar> {
+class _SearchBarState extends ConsumerState<SearchBarCustom> {
   @override
   void initState() {
     // TODO: implement initState
