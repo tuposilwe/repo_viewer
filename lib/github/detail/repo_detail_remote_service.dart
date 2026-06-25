2939549e1e9d1c1f6e1dc8e3cc0bb887745d027b
tuposilwe <rudigermkondya@gmail.com>
2026-06-26 02:57:45 +0300

    Overview of the Repo Detail Feature

---
+++ lib/github/detail/repo_detail_remote_service.dart
@@ -0,0 +1,54 @@
+import 'package:dio/dio.dart';
+import 'package:repo_viewer/core/infrastructure/dio_extensions.dart';
+import 'package:repo_viewer/core/infrastructure/network_exceptions.dart';
+import 'package:repo_viewer/core/infrastructure/remote_response.dart';
+import 'package:repo_viewer/github/core/infrastructure/github_headers.dart';
+import 'package:repo_viewer/github/core/infrastructure/github_headers_cache.dart';
+
+class RepoDetailRemoteService {
+  final Dio _dio;
+  final GithubHeadersCache _headersCache;
+
+  RepoDetailRemoteService(this._dio, this._headersCache);
+
+  Future<RemoteResponse<String>> getReadmeHtml(String fullRepoName) async {
+    final requestUri = Uri.https(
+      'api.github.com',
+      '/repos/$fullRepoName/readme',
+    );
+
+    final previousHeaders = await _headersCache.getHeaders(requestUri);
+
+    try {
+      final response = await _dio.getUri(
+        requestUri,
+        options: Options(
+          headers: {
+            // 'Authorization': 'bearer $token',
+            // 'Accept': acceptHeader,
+            'If-None-Match': previousHeaders?.etag ?? '',
+          },
+          responseType: ResponseType.plain,
+        ),
+      );
+
+      if (response.statusCode == 304) {
+        return const RemoteResponse.notModified(maxPage: 0);
+      } else if (response.statusCode == 200) {
+        final headers = GithubHeaders.parse(response);
+        await _headersCache.saveHeaders(requestUri, headers);
+
+        final html = response.data as String;
+        return RemoteResponse.withNewData(html, maxPage: 0);
+      } else {
+        throw RestAPiException(response.statusCode);
+      }
+    } on DioException catch (e) {
+      if (e.isNoConnectionError) {
+        return const RemoteResponse.noConnection();
+      } else {
+        rethrow;
+      }
+    }
+  }
+}
