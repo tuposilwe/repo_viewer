88acc090b36d8103528d824f6fb861e509295374
tuposilwe <rudigermkondya@gmail.com>
2026-06-26 04:29:55 +0300

    Deleting the Repo Detail Headers

--- lib/github/detail/infrastructure/repo_detail_local_service.dart
+++ lib/github/detail/infrastructure/repo_detail_local_service.dart
@@ -1,4 +1,5 @@
 import 'package:repo_viewer/core/infrastructure/sembast_database.dart';
+import 'package:repo_viewer/github/core/infrastructure/github_headers_cache.dart';
 import 'package:repo_viewer/github/detail/infrastructure/github_repo_detail_dto.dart';
 import 'package:sembast/sembast.dart';
 import 'package:sembast/timestamp.dart';
@@ -8,8 +9,9 @@ class RepoDetailLocalService {
 
   final SembastDatabase _sembastDatabase;
   final _store = stringMapStoreFactory.store('repoDetails');
+  final GithubHeadersCache _headersCache;
 
-  RepoDetailLocalService(this._sembastDatabase);
+  RepoDetailLocalService(this._sembastDatabase, this._headersCache);
 
   Future<void> upsertRepoDetails(
     GithubRepoDetailDto githubRepoDetailDto,
@@ -29,6 +31,9 @@ class RepoDetailLocalService {
       final keysToRemove = keys.sublist(cacheSize);
       for (final key in keysToRemove) {
         await _store.record(key).delete(_sembastDatabase.instance);
+        await _headersCache.deleteHeaders(
+          Uri.https('api.github.com', '/repos/$key/readme'),
+        );
       }
     }
   }
